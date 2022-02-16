4deb682926d540a141544b36f099a303cfbdea1e
Blanc <1yoouoo@gmail.com>
2022-02-17 08:43:24 +0900

    로그인 페이지 만들기 - 3
    local storage를 이용해서 로그인 정보를 웹에 저장해서 리다이렉트 돼도 정보가 그대로 유지되게 하였고 로그아웃할때 그 정보를 null로 받아서 지워버림

--- logInPage/script.js
+++ logInPage/script.js
@@ -1,17 +1,14 @@
 
-function getLoginvalue() {
-    document.getElementById("test").innerHTML = loginValue;  
+function isLogin() {
+    return localStorage.getItem("idKey") !== null;
 }
 
-function checkLogin() {
-    let id_value = document.getElementById("id_text_value").value;
-    let pw_value = document.getElementById("pw_text_value").value;
-    let loginValue = [id_value, pw_value]
+function logout() {
+    console.log("logout");
+    localStorage.removeItem("idKey", null);
+    localStorage.removeItem("pwKey", null);
+}
 
-    if (id_value == '1234' && pw_value == '1234') {
-        window.location.replace('/Practice/Practice/indexPage/index.html')
-    }
-    else {
-        alert('오류!!!')
-    }
+function getPostList() {
+    return [{title: 'jito', content: 'hello'}]
 }
