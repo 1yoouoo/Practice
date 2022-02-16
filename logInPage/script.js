11882a1b210d94a2a8d30277ef8773effc3f5abd
Blanc <1yoouoo@gmail.com>
2022-02-16 15:15:05 +0900

    로그인 페이지 만들기 - 2
    유저한테 정보를 받고 정보가 일치하면 로그인 되면서 인덱스 페이지로 리다이렉트(without new tab)

--- logInPage/script.js
+++ logInPage/script.js
@@ -1,8 +1,17 @@
 
 function getLoginvalue() {
-    let id_value = document.getElementById("id_text_value").value;
-    let pw_value = document.getElementById("pw_text_value").value;
-    let loginValue = [id_value, pw_value]
     document.getElementById("test").innerHTML = loginValue;  
 }
 
+function checkLogin() {
+    let id_value = document.getElementById("id_text_value").value;
+    let pw_value = document.getElementById("pw_text_value").value;
+    let loginValue = [id_value, pw_value]
+
+    if (id_value == '1234' && pw_value == '1234') {
+        window.location.replace('/Practice/Practice/indexPage/index.html')
+    }
+    else {
+        alert('오류!!!')
+    }
+}
